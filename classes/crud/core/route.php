e3c05dd4ad5b11e50502481343e669fb2d0db9b4
Colin Burn-Murdoch <colin@burn-murdoch.com>
2012-06-22 02:20:03 -0700

    Allow exclusion of routes. When you have nested resources, the show resource needs completely disabled, rather than just redirected to edit, so that the nested resource can use that path as its index.

--- classes/crud/core/route.php
+++ classes/crud/core/route.php
@@ -2,7 +2,7 @@
 
 class Crud_Core_Route extends Kohana_Route {
 
-	public static function set_crud($route_name, $uri_prefix, $controller, $skip_show = false,  $guid='id', $guid_regex = Route::REGEX_SEGMENT, $per_page = 50) {
+	public static function set_crud($route_name, $uri_prefix, $controller, $skip_show = false,  $guid='id', $guid_regex = Route::REGEX_SEGMENT, $per_page = 50, $except = array()) {
 		$controller_class = "Controller_{$controller}";
 		if ($controller_class::$order_by) {
 			Route::set("{$route_name}_sort", "{$uri_prefix}/sort")
@@ -22,39 +22,47 @@ class Crud_Core_Route extends Kohana_Route {
 				'order'      => null,
 				'direction'  => 'asc'
 		));
-		Route::set("{$route_name}_new", "{$uri_prefix}/new")
-			->defaults(array(
-				'controller' => $controller,
-				'action'     => 'new',
-				'edit_route' => $route_name . '_edit',
-				'guid'       => $guid,
-		));
-		Route::set("{$route_name}_edit", "{$uri_prefix}/<{$guid}>/edit", array($guid => $guid_regex))
-			->defaults(array(
-				'controller' => $controller,
-				'action'     => 'edit',
-				'edit_route' => $route_name . '_edit',
-				'guid'       => $guid,
-		));
-		
-		if ($skip_show) {
-			Route::set_alias("{$route_name}_edit_alias", "{$uri_prefix}/<{$guid}>", "{$route_name}_edit");
-		} else {
-			Route::set("{$route_name}_show", "{$uri_prefix}/<{$guid}>", array($guid => $guid_regex))
+		if (!in_array('new', $except)) {
+			Route::set("{$route_name}_new", "{$uri_prefix}/new")
 				->defaults(array(
 					'controller' => $controller,
-					'action'     => 'show',
+					'action'     => 'new',
+					'edit_route' => $route_name . '_edit',
+					'guid'       => $guid,
+			));
+		}
+		if (!in_array('edit', $except)) {
+			Route::set("{$route_name}_edit", "{$uri_prefix}/<{$guid}>/edit", array($guid => $guid_regex))
+				->defaults(array(
+					'controller' => $controller,
+					'action'     => 'edit',
+					'edit_route' => $route_name . '_edit',
 					'guid'       => $guid,
 			));
 		}
 		
-		Route::set("{$route_name}_delete", "{$uri_prefix}/<{$guid}>/delete(/<confirm>)", array($guid => $guid_regex, 'confirm' => 'confirm'))
-			->defaults(array(
-				'controller' => $controller,
-				'action'     => 'delete',
-				'index_route'=> $route_name . '_index',
-				'guid'       => $guid,
-		));
+		if (!in_array('show', $except)) {
+			if ($skip_show) {
+				Route::set_alias("{$route_name}_edit_alias", "{$uri_prefix}/<{$guid}>", "{$route_name}_edit");
+			} else {
+				Route::set("{$route_name}_show", "{$uri_prefix}/<{$guid}>", array($guid => $guid_regex))
+					->defaults(array(
+						'controller' => $controller,
+						'action'     => 'show',
+						'guid'       => $guid,
+				));
+			}
+		}
+		
+		if (!in_array('delete', $except)) {
+			Route::set("{$route_name}_delete", "{$uri_prefix}/<{$guid}>/delete(/<confirm>)", array($guid => $guid_regex, 'confirm' => 'confirm'))
+				->defaults(array(
+					'controller' => $controller,
+					'action'     => 'delete',
+					'index_route'=> $route_name . '_index',
+					'guid'       => $guid,
+			));
+		}
 
 	}
 
